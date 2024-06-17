176eb8f23f1b2a5b57dce3368dd8e32751d81624
OpenAI | MERN | Senior Full Stack Developer <141269737+tekdadz@users.noreply.github.com>
2024-06-17 06:40:52 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,50 @@
+<script>
+  import {MaterialApp, AppBar, Col, Row} from 'svelte-materialify';
+  import Table from "./components/Table.svelte";
+  import Info from "./components/Info.svelte";
+  import Error from "./components/Error.svelte";
+  import Graph from "./components/Graph.svelte";
+
+  let theme = 'light';
+
+  async function getEarthquakeData() {
+    const response = await fetch("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&minmagnitude=5");
+    let data = await response.json();
+    return data.features;
+  }
+
+  let promise = getEarthquakeData();
+
+  var innerHeight;
+</script>
+
+<svelte:window bind:innerHeight={innerHeight} />
+
+<MaterialApp {theme}>
+  <AppBar>
+    <span slot="title">USGS Earthquakes</span>
+  </AppBar>
+  <div class="container">
+    <Row class="center" noGutters style="height: {innerHeight * 0.8}px">
+    {#await promise}
+      <Col><Info msg={'Data is loading...'}/></Col>
+    {:then data}
+      <Col class="pa-2"><Table {data}/></Col>
+      <Col class="pa-2"><Graph {data}/></Col>
+    {:catch error}
+      <Col><Error msg={'Data cannot be loaded.'}/></Col>
+    {/await}
+    </Row>
+    <Row>
+      <Col>Data is fetched from USGS: https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&minmagnitude=5</Col>
+    </Row>
+  </div>
+</MaterialApp>
+
+<style>
+  .container {
+    padding: 8px;
+    margin-bottom: 12px;
+    background-color: var(--theme-app-bar);
+  }
+</style>
